ad0ff863ab5e3ad753a3f8d8b5a6344493734c7d
Will Diable <wtdiable@icloud.com>
2022-01-27 16:39:03 +0000

    empty journeys hash

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,13 +5,15 @@ class Oystercard
   MIN_BALANCE = 1
   attr_reader :in_journey
   attr_reader :entry_station
+  attr_reader :journeys
 
   def initialize
     @balance = 0
+    @journeys = {}
   end
 
   def top_up(amount)
-    fail "Maximum balance reached" if @balance >= MAX_BALANCE
+    fail "Maximum balance reached" if @balance + amount > MAX_BALANCE
     @balance += amount
     "#{@balance} added to account"
   end
@@ -22,7 +24,6 @@ class Oystercard
 
   def touch_in(station)
     @entry_station = station
-    @in_journey = true
   end
 
   def touch_out
@@ -37,12 +38,18 @@ class Oystercard
     @balance -= amount
     "#{amount} deducted from account"
   end
+
+  # def add_entry(day, note)
+  #   fail "Unlock diary first" if @locked
+  #   @diary[day] = note
+  #   return "Entry added successfully"
+  # end
+
+  # def get_entry(day)
+  #   fail "Unlock diary first" if @locked
+  #   @diary[day]
+  # end
+  
   
 end
-
-p card = Oystercard.new
-p card.top_up(10)
-p card.touch_in("Preston")
-p card.entry_station
-
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,77 +1,62 @@
 require 'oystercard'
 
-
-
 describe Oystercard do
   
-  let(:station){ double :station }
+  let(:entry_station){ double 'Preston' }
+
+  # This also works: let(:entry_station){ double :entry_station }
   
   card = Oystercard.new
   it {is_expected.to respond_to :balance}
 
   it 'checks balance' do
-    card = Oystercard.new
-    expect(card.balance).to eq 0
+    expect(subject.balance).to eq 0
   end
 
   it 'can top-up balance' do
-    card = Oystercard.new
-    expect(card.top_up(20)).to eq "20 added to account"
+    expect(subject.top_up(20)).to eq "20 added to account"
   end
 
   it 'has a maximum balance of £90' do
-    card = Oystercard.new
-    # initial deposit
-    card.top_up(90)
-    # second deposit
-    expect{card.top_up(20)}.to raise_error "Maximum balance reached"
+    expect{subject.top_up(91)}.to raise_error "Maximum balance reached"
   end
 
-  # it 'can deduct money' do
-  #   card = Oystercard.new
-  #   card.top_up(60)
-  #   expect(card.deduct(20)).to eq "20 deducted from account"
-  # end
-
   it 'can check customer is on a journey' do
-    card = Oystercard.new
-    expect(card.in_journey?).to eq false
+    expect(subject.in_journey?).to eq false
   end
 
   it 'can check customer is not on a journey' do
-    card = Oystercard.new
-    card.touch_in("Preston")
-    expect(card.in_journey?).to eq true
+    subject.touch_in(entry_station)
+    expect(subject.in_journey?).to eq true
   end
 
   it 'can touch a user in' do
-    card = Oystercard.new
-    expect(card.touch_in("Preston")).to eq true
+    expect(subject.touch_in(entry_station)).to eq entry_station
   end
 
   it 'can touch a user out' do
-    card = Oystercard.new
-    card.top_up(20)
-    expect(card.touch_out).to eq "Thank you for riding with us today"
+    subject.top_up(20)
+    expect(subject.touch_out).to eq "Thank you for riding with us today"
   end
 
   it 'has a minimum balance of £1' do
-    card = Oystercard.new
-    expect{card.touch_out}.to raise_error "insufficient funds"
+    expect{subject.touch_out}.to raise_error "insufficient funds"
   end
 
 
   it 'can deduct money on touch out' do
-    card = Oystercard.new
     subject.top_up(10)
-    card.touch_in("Preston")
+    subject.touch_in(entry_station)
     expect{subject.touch_out}.to change {subject.balance}.by(-Oystercard::MIN_BALANCE)
   end
 
-  it 'saves starting station' do
-    card = Oystercard.new
-    subject.touch_in("Preston")
-    expect(subject.entry_station).to eq "Preston"
+  it 'saves entry station' do
+    subject.touch_in(entry_station)
+    expect(subject.entry_station).to eq entry_station
+  end
+
+  it 'begins with an empty journey list' do
+    expect(subject.journeys).to be_empty
   end
 
 end
